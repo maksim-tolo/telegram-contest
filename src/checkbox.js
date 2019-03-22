640f7df2cee5fa91f8380a6168c570ff17f83b43
Maxim Tolochko <maksim.tolo@gmail.com>
2019-03-22 21:59:22 +0300

    Add checkbox component

---
+++ src/checkbox.js
@@ -0,0 +1,50 @@
+export default class Checkbox {
+  static get DEFAULT_OPTIONS() {
+    return {
+      color: '#000',
+      label: '',
+      onChange() {}
+    };
+  }
+
+  constructor(options = {}) {
+    this.options = Object.assign({}, Checkbox.DEFAULT_OPTIONS, options);
+    this.root = document.createElement('div');
+  }
+
+  addListeners() {
+    this.root.querySelector('.input')
+      .addEventListener('change', this.options.onChange);
+  }
+
+  removeListeners() {
+    this.root.querySelector('.input')
+      .removeEventListener('change', this.options.onChange);
+  }
+
+  render() {
+    const { label, color } = this.options;
+
+    this.root.innerHTML = `
+      <div class="container">
+        <label class="checkbox">
+            <input type="checkbox" class="input" checked />
+            <span class="tick" style="border-color: ${color}"></span>
+            <span class="label">${label}</span>
+        </label>
+      </div>
+    `;
+  }
+
+  attach(node) {
+    this.render();
+    node.appendChild(this.root);
+    this.addListeners();
+  }
+
+  detach(node) {
+    node.removeChild(this.root);
+    this.removeListeners();
+    this.root = null;
+  }
+}
