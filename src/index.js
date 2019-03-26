bd3504f79aaf3bee36f91d01835d5578fb44ac49
Maxim Tolochko <maksim.tolo@gmail.com>
2019-03-26 21:11:59 +0300

    Move night mode switcher to a separate folder

--- src/index.js
+++ src/index.js
@@ -1,64 +1,17 @@
 import SvgChart from './svg-chart';
+import NightModeSwitcher from './night-mode-switcher';
 
 import chartData from './chart-data';
 
 import './index.css';
 
-const darkModeKey = 'darkMode';
-
-function isDarkMode() {
-  if (localStorage) {
-    const value = localStorage.getItem(darkModeKey);
-
-    if (value) {
-      return JSON.parse(value);
-    }
-  }
-
-  return false;
-}
-
-function saveTheme(isDark) {
-  if (localStorage) {
-    localStorage.setItem(darkModeKey, isDark);
-  }
-}
-
-function toggleMod(isDark, nightModeSwitcher) {
-  if (isDark) {
-    document.body.className = 'darkMode';
-    nightModeSwitcher.textContent = 'Switch to Day Mode';
-  } else {
-    document.body.className = '';
-    nightModeSwitcher.textContent = 'Switch to Night Mode';
-  }
-}
-
-function initNightModeSwitcher() {
-  const nightModeSwitcher = document.createElement('div');
-
-  nightModeSwitcher.className = 'nightModeSwitcher';
-
-  let isDark = isDarkMode();
-
-  toggleMod(isDark, nightModeSwitcher);
-
-  nightModeSwitcher.addEventListener('click', () => {
-    isDark = !isDark;
-
-    toggleMod(isDark, nightModeSwitcher);
-    saveTheme(isDark);
-  });
-
-  return nightModeSwitcher;
-}
+const toggleDarkMode = isDarkMode =>
+  document.body.className = isDarkMode ? 'darkMode' : '';
 
 function init() {
+  const nightModeSwitcher = new NightModeSwitcher({ onToggle: toggleDarkMode });
   const root = document.getElementById('root');
   const title = document.createElement('div');
-  const nightModeSwitcher = initNightModeSwitcher();
-
-  const width = Math.min(root.scrollWidth, 1024);
 
   title.textContent = 'Followers';
   title.className = 'title';
@@ -67,7 +20,7 @@ function init() {
 
   chartData.forEach((data) => {
     const chart = new SvgChart(data, {
-      width,
+      width: Math.min(root.scrollWidth, 1024),
       brushHeight: 64,
       height: 512
     });
@@ -75,7 +28,7 @@ function init() {
     chart.attach(root);
   });
 
-  root.appendChild(nightModeSwitcher);
+  nightModeSwitcher.attach(root);
 }
 
 window.addEventListener('load', init);
---
+++ src/night-mode-switcher/index.js
@@ -0,0 +1,77 @@
+import { classNames } from '../helper';
+
+import styles from './index.module.css';
+
+const cx = classNames.bind(styles);
+
+export default class NightModeSwitcher {
+  static get DEFAULT_OPTIONS() {
+    return {
+      saveKey: 'darkMode',
+      onToggle() {}
+    };
+  }
+
+  static getSavedTheme(key) {
+    if (localStorage) {
+      const value = localStorage.getItem(key);
+
+      if (value) {
+        return JSON.parse(value);
+      }
+
+      return matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    return false;
+  }
+
+  static saveTheme(key, isDarkMode) {
+    if (localStorage) {
+      localStorage.setItem(key, isDarkMode);
+    }
+  }
+
+  constructor(options) {
+    this.options = Object.assign({}, NightModeSwitcher.DEFAULT_OPTIONS, options);
+    this.root = document.createElement('div');
+    this.root.className = cx('nightModeSwitcher');
+    this.isDarkMode = NightModeSwitcher.getSavedTheme(this.options.saveKey);
+    this.updateDom();
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+
+  updateDom() {
+    this.root.textContent = this.isDarkMode ? 'Switch to Day Mode' : 'Switch to Night Mode';
+
+    if (this.options.onToggle) {
+      this.options.onToggle(this.isDarkMode);
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+
+    this.updateDom();
+    NightModeSwitcher.saveTheme(this.options.saveKey, this.isDarkMode);
+  }
+
+  addListeners() {
+    this.root.addEventListener('click', this.toggleTheme);
+  }
+
+  removeListeners() {
+    this.root.removeEventListener('click', this.toggleTheme);
+  }
+
+  attach(node) {
+    node.appendChild(this.root);
+    this.addListeners();
+  }
+
+  detach(node) {
+    node.removeChild(this.root);
+    this.removeListeners();
+    this.root = null;
+  }
+}
